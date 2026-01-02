819e0746988272a8aba970dcdb3270561f063e72
Tendoskria <theo200087@gmail.com>
2026-01-02 01:49:41 +0100

    init commit

---
+++ src/main/java/fr/mendes/itemreader/ItemReaderModClient.java
@@ -0,0 +1,10 @@
+package fr.mendes.itemreader;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItemReaderModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/fr/mendes/itemreader/ItemReaderModDataGenerator.java
@@ -0,0 +1,11 @@
+package fr.mendes.itemreader;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ItemReaderModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
